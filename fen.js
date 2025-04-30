c52ae2677eb10522e8340a1065cdd65d2564d328
Andreas Friestad <afriestad@users.noreply.github.com>
2025-04-30 18:37:59 +0200

    Add parsing

--- fen.js
+++ fen.js
@@ -1,3 +1,11 @@
+const colNames = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+function isFen(maybeFen) {
+    pattern = /^([0-9bknpqrBKNPQR]{1,8}\/){7}[0-9bknpqrBKNPQR]{1,8}$/gm
+
+    return pattern.test(maybeFen);
+}
+
 function makeFen() {
     const cells = Array.from(document.querySelectorAll(".cell"))
 
@@ -5,7 +13,7 @@ function makeFen() {
     for (let i = 8; i > 0; i--) {
         const rowCells = cells.filter(el => el.id.includes(i.toString()));
 
-        const row = ["a", "b", "c", "d", "e", "f", "g", "h"].map(
+        const row = colNames.map(
             c => {
                 const cell = rowCells.find(el => el.id.includes(c));
                 if (cell) {
@@ -36,4 +44,42 @@ function makeFen() {
     }
 
     document.getElementById("fen").textContent = fenList.join("/")
+}
+
+function parseFen() {
+    const fenInput = document.getElementById("fenInput");
+    const fenError = document.getElementById("fenError");
+
+    const fenString = fenInput.value;
+
+    if (!isFen(fenString)) {
+        fenError.textContent = `${fenString} is not a valid FEN`;
+        fenInput.value = "";
+
+        return;
+    }
+
+    resetBoard();
+    fenError.textContent = "";
+
+    fenString.split("/").forEach((fenLine, index) => {
+        const rowIndex = 8-index;
+        let colIndex = 0;
+        for (const fenChar of fenLine) {
+            if (isNaN(parseInt(fenChar))) {
+                const id = `${colNames[colIndex]}${rowIndex}`;
+                const cell = document.getElementById(id);
+
+                if (cell) {
+                    myglobals.piece = fenChar;
+                    updateImage(cell);
+                }
+                colIndex += 1;
+            } else {
+                colIndex += parseInt(fenChar);
+            }
+        }
+    });
+
+    makeFen()
 }
